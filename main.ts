80e51d7877d66f09efd2ca574004e8d11d273894
IPedr4o <122974238+IPedr4o@users.noreply.github.com>
2023-01-18 10:33:00 +0000

    Atualização main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,15 +2,15 @@ input.onSound(DetectedSound.Loud, function () {
     Palmas = !(Palmas)
     if (Palmas) {
         basic.showLeds(`
+            # . # . #
             . # . # .
+            # . # . #
             . # . # .
-            . . . . .
-            # . . . #
-            . # # # .
+            # . # . #
             `)
     } else {
         basic.clearScreen()
     }
 })
 let Palmas = false
-input.setSoundThreshold(SoundThreshold.Loud, 128)
+input.setSoundThreshold(SoundThreshold.Loud, 132)
