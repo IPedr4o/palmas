50bf54c243d1c34b2ff6b5a9c6e4139394c37e58
IPedr4o <122974238+IPedr4o@users.noreply.github.com>
2023-01-18 10:25:42 +0000

    Ficheiros iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onSound(DetectedSound.Loud, function () {
+    Palmas = !(Palmas)
+    if (Palmas) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        basic.clearScreen()
+    }
+})
+let Palmas = false
+input.setSoundThreshold(SoundThreshold.Loud, 128)
